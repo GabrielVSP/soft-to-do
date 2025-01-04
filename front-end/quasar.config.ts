c9c052fc7fa2149710212d1d001911aeeb1d7522
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2025-01-04 20:43:37 -0300

    Update: app name(front end -> To do app)

--- front-end/quasar.config.ts
+++ front-end/quasar.config.ts
@@ -204,7 +204,7 @@ export default defineConfig((ctx) => {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'front-end'
+        appId: 'To do App'
       }
     },
 
